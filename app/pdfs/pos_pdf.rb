98908425cfd0aac62b17c6e05248224ae112828e
adjidedjo <adji.el.djo@gmail.com>
2015-03-05 12:49:54 +0700

    revise number to currency pdf

--- app/pdfs/pos_pdf.rb
+++ app/pdfs/pos_pdf.rb
@@ -1,4 +1,5 @@
 class PosPdf < Prawn::Document
+  include ActionView::Helpers::NumberHelper
   def initialize(order, order_no)
     super({:page_size => 'A4'})
     @order = order
@@ -83,12 +84,19 @@ class PosPdf < Prawn::Document
         text "Alamat Kirim          : #{@order.alamat_kirim}", :size => 10, :style => :bold
       end
     end
+    bounding_box([350, cursor + 45], :width => 250) do
+      text "Total       : Rp. ", :size => 10, :style => :bold
+      move_down 5
+      text "Bayar     : Rp. ", :size => 10, :style => :bold
+      move_down 5
+      text "Sisa        : Rp. ", :size => 10, :style => :bold
+    end
     bounding_box([250, cursor + 45], :width => 250) do
-      text "Total       : #{@order.netto}", :size => 10, :style => :bold, :align => :right
+      text "#{number_to_currency(@order.netto, precision:0, unit: "", separator: ".", delimiter: ".")}", :size => 10, :style => :bold, :align => :right
       move_down 5
-      text "Bayar      : #{@order.pembayaran}", :size => 10, :style => :bold, :align => :right
+      text "#{number_to_currency(@order.pembayaran, precision:0, unit: "", separator: ".", delimiter: ".")}", :size => 10, :style => :bold, :align => :right
       move_down 5
-      text "Sisa        : #{@order.netto-@order.pembayaran}", :size => 10, :style => :bold, :align => :right
+      text "#{number_to_currency((@order.netto-@order.pembayaran), precision:0, unit: "", separator: ".", delimiter: ".")}", :size => 10, :style => :bold, :align => :right
     end
 
     stroke do
