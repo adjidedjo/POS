8895dc1991c48bf4938fef03dd8aa52651a6feec
adjidedjo <adji.el.djo@gmail.com>
2015-03-11 08:42:52 +0700

    revise validation tanggal_kirim

--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -2,9 +2,15 @@ class SaleItem < ActiveRecord::Base
   belongs_to :sale
   belongs_to :user
 
+  validates :tanggal_kirim, presence: true, unless: :taken?
+
   before_create do
     if taken == true
       self.tanggal_kirim = Date.today
+    end
+
+    def taken?
+      taken?
     end
 #    no_sale = self.sale.no_sale.to_s.rjust(4, '0')
 #    bulan = Date.today.strftime('%m')
