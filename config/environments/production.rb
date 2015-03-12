6b059dc29818b4a13f8912eeee145349f5e291d4
adjidedjo <adji.el.djo@gmail.com>
2015-03-12 13:59:48 +0700

    add configuration to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '192.168.101.220:1107' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.ras.co.id",
+    :port                 => 25,
+    :domain               => 'ras.co.id',
+    :enable_starttls_auto => true  }
 end
