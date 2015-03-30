ac7cbddd1a61c030c284db7ac56697ac2ededf40
adjidedjo <adji.el.djo@gmail.com>
2015-03-30 20:06:00 +0700

    revise routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   resources :sales do
     collection do
+      get "get_mid_from_merchant"
+      get "get_second_mid_from_merchant"
       get "get_kode_barang_from_serial"
       get "edit_by_confirmation"
       get "destroy_by_confirmation"
