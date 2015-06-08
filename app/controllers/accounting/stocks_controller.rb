b42fa70e5af96f141de95dfb2c9310cd4331244a
adjidedjo <adji.el.djo@gmail.com>
2015-06-08 14:38:59 +0700

    revise report for accounting

--- app/controllers/accounting/stocks_controller.rb
+++ app/controllers/accounting/stocks_controller.rb
@@ -43,14 +43,6 @@ class Accounting::StocksController < ApplicationController
   end
 
   def mutasi_stock
-    @channel_customer = []
-    channel = current_user.branch.sales_counters
-    if channel.present?
-      current_user.branch.sales_counters.each do |sc|
-        sc.recipients.group(:channel_customer_id, :sales_counter_id, :brand_id).each do |scr|
-          @channel_customer << scr.channel_customer
-        end
-      end
+    @channel_customer = current_user.channel_customer
     end
   end
-end
