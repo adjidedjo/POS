8aedf18a6b9127c0c3a07bc144b569c76f35a023
adjidedjo <adji.el.djo@gmail.com>
2016-03-02 11:51:43 +0700

    revising jde item master

--- app/models/jde_item_master.rb
+++ app/models/jde_item_master.rb
@@ -1,19 +1,19 @@
 class JdeItemMaster < ActiveRecord::Base
-  establish_connection "jdeoracle"
-  self.table_name = "proddta.f4101" #im
-
-  def self.find_item_number(item)
-    item_number = where("imaitm like ?", "#{item}%")
-    item_number.empty? ? 0 : item_number.first.imlitm
-  end
-
-  def self.find_item_desc1(item)
-    item_number = where("imaitm like ?", "#{item}%")
-    item_number.empty? ? 0 : item_number.first.imdsc1
-  end
-
-  def self.find_item_desc2(item)
-    item_number = where("imaitm like ?", "#{item}%")
-    item_number.empty? ? 0 : item_number.first.imdsc2
-  end
+#  establish_connection "jdeoracle"
+#  self.table_name = "proddta.f4101" #im
+#
+#  def self.find_item_number(item)
+#    item_number = where("imaitm like ?", "#{item}%")
+#    item_number.empty? ? 0 : item_number.first.imlitm
+#  end
+#
+#  def self.find_item_desc1(item)
+#    item_number = where("imaitm like ?", "#{item}%")
+#    item_number.empty? ? 0 : item_number.first.imdsc1
+#  end
+#
+#  def self.find_item_desc2(item)
+#    item_number = where("imaitm like ?", "#{item}%")
+#    item_number.empty? ? 0 : item_number.first.imdsc2
+#  end
 end
