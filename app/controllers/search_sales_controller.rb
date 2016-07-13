9dd2a761f97d54b6c5af57c74aed02a9109569df
adjidedjo <adji.el.djo@gmail.com>
2016-07-13 11:23:30 +0700

    revise sales reports

--- app/controllers/search_sales_controller.rb
+++ app/controllers/search_sales_controller.rb
@@ -58,7 +58,7 @@ class SearchSalesController < ApplicationController
          kode_barang not like ? and kode_barang not like ? and cancel = ? and channel_customer_id = ?",
           @search.dari_tanggal, @search.sampai_tanggal, "#{'E'}%", "#{'L'}%", 0, @cc.id)
         .group(:kode_barang).order('sum_jumlah DESC').limit(10)
-        @top_pc = @cc.sales.select('*, sales_promotion_id, sum(netto_elite) as elite, sum(netto_lady) as lady')
+        @top_pc = @cc.sales.select('*, sales_promotion_id, sum(netto_elite) as elite, sum(netto_lady) as lady, sum(netto_serenity) as serenity, sum(netto_tech) as tech')
         .where("date(created_at) >= ? and date(created_at) <= ? and cancel_order = ?",
           @search.dari_tanggal, @search.sampai_tanggal,0).group(:sales_promotion_id).order('elite DESC, lady DESC')
       else
